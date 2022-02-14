64d9edfb32488cbe01104b70e6327adde4537b76
Adam Gardner <adam@agardner.net>
2022-02-14 15:54:04 +1000

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -12,18 +12,18 @@ sed -i "s/^    server:.*/    server: https:\/\/$MASTER_IP:6443/" $HOME/.kube/con
 echo "-- Waiting for Nodes to Signal Ready (timeout 120s) --"
 kubectl wait --for=condition=ready nodes --all --timeout=120s
 
-#echo "-- Installing Keptn via Helm --"
+echo "-- Installing Keptn via Helm --"
 helm install keptn https://github.com/keptn/keptn/releases/download/0.12.0/keptn-0.12.0.tgz -n keptn --create-namespace
 
 echo "-- Installing Job Executor Service --"
 helm install -n keptn job-executor-service https://github.com/keptn-contrib/job-executor-service/releases/download/0.1.6/job-executor-service-0.1.6.tgz
 
-echo "-- Wait for all pods in Keptn namespace to signal ready. Timeout=10 mins --"
-kubectl -n keptn wait --for=condition=ready pods --all --timeout=10m
+echo "-- Wait for all pods in Keptn namespace to signal ready. Timeout=20 mins --"
+kubectl -n keptn wait --for=condition=ready pods --all --timeout=20m
 
 echo "-- Expose Keptn to http://localhost on port 80 --"
 # Patch api-gateway-nginx to include route from NodePort 31090 to 8080
-# Remember that 31090 will then be mapped to 80 in hte kind.yaml file
+# Remember that 31090 will then be mapped to 80 in the kind.yaml file
 # So we can get to keptn from laptop on http://localhost
 # If inside the docker container, we need to use NodePort IP and port combo for example
 # export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
@@ -37,7 +37,7 @@ echo "-- Restart Keptn Bridge to load new settings --"
 kubectl -n keptn delete pods --selector=app.kubernetes.io/name=bridge --wait
 
 #echo "-- Authenticating keptn CLI"
-export NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
+NODE_IP=$(kubectl get nodes --namespace default -o jsonpath="{.items[0].status.addresses[0].address}")
 keptn auth --endpoint=http://$NODE_IP:31090 --api-token=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
 
 echo "-- Create Keptn Hello World Project --"
